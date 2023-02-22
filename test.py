36071494b6c847e8fb577ac769208c087dc80c41
shubham-kanodia <shubhamkanodia123@gmail.com>
2023-02-22 12:13:25 +0530

    Modify test

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 import hashlib
 
-correct_solution_hash = "051701cb2b91513339363104e6677a0d"
-solution_hash = hashlib.md5(open("solution.csv", "rb").read()).hexdigest()
+correct_solution_hash = "24112618bfab80fafe718683fd38b901"
+solution_hash = hashlib.md5(open("solution.csv", "r").read().encode()).hexdigest()
 
 assert(solution_hash == correct_solution_hash)
