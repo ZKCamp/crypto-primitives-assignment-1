bc55f5afba3e0bd65b47c55fcf60b7dcd2f666ff
shubham-kanodia <shubhamkanodia123@gmail.com>
2023-02-22 12:11:59 +0530

    Modify test hash

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 
 import hashlib
 
-correct_solution_hash = "14fe11c40e4275e968186cf4fc2e2ebc"
+correct_solution_hash = "051701cb2b91513339363104e6677a0d"
 solution_hash = hashlib.md5(open("solution.csv", "rb").read()).hexdigest()
 
 assert(solution_hash == correct_solution_hash)
