6576300163dce93200eb3d2afe9267c1d29820e2
shubham-kanodia <shubhamkanodia123@gmail.com>
2023-02-22 11:51:54 +0530

    Add test

---
+++ test.py
@@ -0,0 +1,8 @@
+# Do not change the code in this file
+
+import hashlib
+
+correct_solution_hash = "14fe11c40e4275e968186cf4fc2e2ebc"
+solution_hash = hashlib.md5(open("solution.csv", "rb").read()).hexdigest()
+
+assert(solution_hash == correct_solution_hash, "Incorrect solution, try again!")
